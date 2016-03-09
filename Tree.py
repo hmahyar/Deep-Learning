bffc93575c1c4d482ff1e812e387a9afab64d415
hmahyar <hooman.mahyar@gmail.com>
2016-03-08 17:34:25 -0800

    feat: added Min() and Max() Functions for BinaryTreeClass

--- Tree.py
+++ Tree.py
@@ -26,49 +26,24 @@ class BinaryTree(object):
         else:
             self.binery_insert(node,tree[2])
 
-class binaryHeap(object):
-    def __init__(self):
-        self.heapList= [0]
-    
-    def sync_tree(self,tree_len):
-        while tree_len>0:
-            #print 'Check',tree_len,' ',(tree_len)//2
-            if self.heapList[tree_len]<self.heapList[(tree_len)//2]:
-                self.heapList[tree_len],self.heapList[(tree_len)//2]=self.heapList[(tree_len)//2],self.heapList[tree_len]
-                #print 'Swap'
-            tree_len = (tree_len//2)
-
-    def insert(self,node):
-        self.heapList.append(node)
-        self.sync_tree(len(self.heapList)-1)
-        
-    def print_tree(self):
-        print self.heapList
-
-    def min(self):
-        return self.heapList[1]
-    def max(self,i=1):
-        if i*2<len(self.heapList)-1:
-            return self.max(i*2)
+    def max(self,tree):
+        if tree[2]:
+            return self.max(tree[2])
         else:
-            if self.heapList[i]>self.heapList[i+1]:
-                return self.heapList[i]
-            else:
-                return self.heapList[i+1]
+            return tree[0] 
+    
+    def min(self,tree):
+        if tree[1]:
+            return self.min(tree[1])
+        else:
+            return tree[0] 
 
 T = BinaryTree(5)
 for i in [3,7,1,2,6,9,8,10,4]:
     T.binery_insert(i,T.tree)
 #T.traverse_tree(T.tree)
-
-B = binaryHeap()
-for i in [7,3,1,2,6,9,8,10,4]:
-    B.insert(i)
-    
-
-print 'Min: ',B.min()
-print 'Max: ',B.max()
-
+print 'max: ',T.max(T.tree)
+print 'min: ',T.min(T.tree)
 
 
 
